b871c88fb419ba6331c88c613384d352b3f5d3ac
evsasse <evsasse@gmail.com>
2020-06-08 19:40:38 -0300

    adding peeps

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 const HomePage = () => (
-  <main className="container mx-auto p-5 my-8">
-    <div className="custom-shape-divider-bottom-1591633768">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M892.25 114.72L0 0v120h1200V0L892.25 114.72z" className="shape-fill"></path>
-      </svg>
-    </div>
-
-    <header className="text-center my-32">
-      <img src="/1313labs.svg"></img>
+  <main className="container mx-auto">
+    <header className="text-center p-5 mt-16">
+      <img src="/1313labs.svg" />
     </header>
+
+    <footer className="footer">
+      <div className="peeps">
+        <img src="/sasse.svg" />
+        <img src="/ze.svg" />
+        <img src="/bordi.svg" />
+        <img src="/feijo.svg" />
+      </div>
+    </footer>
   </main>
 );
 
