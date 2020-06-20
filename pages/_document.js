f2ccfc87385d86d91b1e29bf1f8fe8252090dc39
Evandro Sasse <evsasse@gmail.com>
2020-06-20 15:36:55 -0300

    4 bagckground themes

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const bgOptions = ["main-bg--summer", "main-bg--winter", "main-bg--spring", "main-bg--fall"];
+
 class CustomDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -8,11 +10,13 @@ class CustomDocument extends Document {
   }
 
   render() {
+    const bgRandomOption = bgOptions[parseInt(bgOptions.length * Math.random())];
+
     return (
       <Html lang="en">
         <Head />
 
-        <body>
+        <body className={`main-bg ${bgRandomOption}`}>
           <Main />
           <NextScript />
         </body>
