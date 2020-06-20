1abe372d835de20ead6ce1c82bbdc4b5f7078bd9
Evandro Sasse <evsasse@gmail.com>
2020-06-20 15:36:23 -0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e8aab5f1856037d20285";
+/******/ 	var hotCurrentHash = "09740c6af3a4c337caa8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
